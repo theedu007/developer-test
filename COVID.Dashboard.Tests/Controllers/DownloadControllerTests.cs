efaf58e441461b46132a7e547d3d93db645d2c7c
Edu Arevalo <eabigboy4@gmail.com>
2021-01-09 23:43:45 -0600

    Added unit test project

---
+++ COVID.Dashboard.Tests/Controllers/DownloadControllerTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace COVID.Dashboard.Tests.Controllers
+{
+    [TestFixture]
+    public class DownloadControllerTests
+    {
+    }
+}
---
+++ COVID.Dashboard.Tests/Services/ReportServiceTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace COVID.Dashboard.Tests.Services
+{
+    [TestFixture]
+    class ReportServiceTests
+    {
+    }
+}
